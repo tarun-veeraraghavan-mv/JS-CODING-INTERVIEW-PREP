79463a99e250abf21cd945b9e181e93e51ede106
tarun-veeraraghavan-mv <tarun.veeraraghavan.dev@gmail.com>
2025-02-14 15:39:27 -0700

    Initial commit

---
+++ reversing/reverse-a-string.js
@@ -0,0 +1,16 @@
+// Directions
+// Given a string, return a new string with a reversed order of charecters
+
+// TEST CASES:
+// reverse('apple') 
+// reverse('hello')
+
+// EXPECTED OUTPUT:
+// 'elppa'
+// 'olleh'
+
+// Soln 1:
+
+function reverse1(str) {
+  
+}
